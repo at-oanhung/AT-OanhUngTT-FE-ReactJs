7cdc3afe3589175250c1667579c9f7ea479e7be0
at-oanhung <oanh.ung@asiantech.vn>
2020-04-03 11:18:49 +0700

    insert scss

--- todo-list/src/index.js
+++ todo-list/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import './assets/scss/styles.scss';
+import './styles/styles.scss';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
